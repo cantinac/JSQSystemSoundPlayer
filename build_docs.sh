725a40100e858cbcc60e3a3f0093e0f20dff6de1
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-30 12:30:47 -0700

    update build docs script. gen docs.

--- build_docs.sh
+++ build_docs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 appledoc --create-html --no-install-docset --docset-install-path ./_docs --project-name JSQSystemSoundPlayer --project-version 3.0.0 --project-company "Jesse Squires" --company-id com.jessesquires --output ./_docs JSQSystemSoundPlayer/JSQSystemSoundPlayer/JSQSystemSoundPlayer/
+
+cp -rf _docs/com.jessesquires.JSQSystemSoundPlayer.docset/Contents/Resources/Documents/* .
